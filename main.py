ddb57a225e08b9a4a04a27e7da17141bde7ea3a8
Nozila <nozilarajabboyeva@gmail.com>
2026-03-27 17:20:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#1-misol
+m1 = lambda x: x * 2
+
+res = m1(8272)
+print(res)
+
+#2-misol
+m2 = lambda x: x ** 2
+result1 = m2(5)
+print(result1)
+
+#3-misol
+m3 = lambda x: x ** 3
+result1 = m3(3)
+print(result1)
+
+#4-misol
+m4 = lambda a, b: a + b
+result1 = m4(10, 4)
+print(result1)
+
+#5-misol
+m5 = lambda a, b: a - b
+result1 = m5(15, 4)
+print(result1)
